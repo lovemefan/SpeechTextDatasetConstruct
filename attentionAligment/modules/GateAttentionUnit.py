26dad8bf37b29642179f738e12b2a8c305ee4344
lovemefan <450489712@qq.com>
2022-04-01 16:27:29 +0800

    add gate attention unit

---
+++ attentionAligment/modules/GateAttentionUnit.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+# @Time  : 2022/3/31 12:13
+# @Author : lovemefan
+# @Email : lovemefan@outlook.com
+# @File : GateAttentionUnit.py
+import math
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+from einops import rearrange
+
+
+# helper functions
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+def padding_to_multiple_of(n, mult):
+    remainder = n % mult
+    if remainder == 0:
+        return 0
+    return mult - remainder
+
+# scalenorm
+
+class ScaleNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-5):
+        super().__init__()
+        self.scale = dim ** -0.5
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1))
+
+    def forward(self, x):
+        norm = torch.norm(x, dim = -1, keepdim = True) * self.scale
+        return x / norm.clamp(min = self.eps) * self.g
+
+# absolute positional encodings
+
+
+
+
+
+# class
+
+class OffsetScale(nn.Module):
+    def __init__(self, dim, heads = 1):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(heads, dim))
+        self.beta = nn.Parameter(torch.zeros(heads, dim))
+        nn.init.normal_(self.gamma, std = 0.02)
+
+    def forward(self, x):
+        out = einsum('... d, h d -> ... h d', x, self.gamma) + self.beta
+        return out.unbind(dim = -2)
+
+# gated attention unit
+
+class GAU(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        query_key_dim = 128,
+        expansion_factor = 2.,
+        add_residual = True,
+        causal = False,
+        dropout = 0.,
+        norm_klass = nn.LayerNorm
+    ):
+        super().__init__()
+        hidden_dim = int(expansion_factor * dim)
+
+        self.norm = norm_klass(dim)
+        self.causal = causal
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_hidden = nn.Sequential(
+            nn.Linear(dim, hidden_dim * 2),
+            nn.SiLU()
+        )
+
+        self.to_qk = nn.Sequential(
+            nn.Linear(dim, query_key_dim),
+            nn.SiLU()
+        )
+
+        self.offsetscale = OffsetScale(query_key_dim, heads = 2)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+        self.add_residual = add_residual
+
+    def forward(
+        self,
+        x,
+        rel_pos_bias = None,
+        mask = None
+    ):
+        seq_len, device = x.shape[-2], x.device
+
+        normed_x = self.norm(x)
+        v, gate = self.to_hidden(normed_x).chunk(2, dim = -1)
+
+        qk = self.to_qk(normed_x)
+        q, k = self.offsetscale(qk)
+
+        sim = einsum('b i d, b j d -> b i j', q, k) / seq_len
+
+        if exists(rel_pos_bias):
+            sim = sim + rel_pos_bias
+
+        attn = F.relu(sim) ** 2
+        attn = self.dropout(attn)
+
+        if exists(mask):
+            mask = rearrange(mask, 'b j -> b 1 j')
+            attn = attn.masked_fill(~mask, 0.)
+
+        if self.causal:
+            causal_mask = torch.ones((seq_len, seq_len), dtype = torch.bool, device = device).triu(1)
+            attn = attn.masked_fill(causal_mask, 0.)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = out * gate
+
+        out = self.to_out(out)
+
+        if self.add_residual:
+            out = out + x
+
+        return out
+
+
