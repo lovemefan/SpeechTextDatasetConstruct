2fb08ae74b96a8ceef6614eca937a1be66ebe267
lovemefan <450489712@qq.com>
2022-04-08 21:43:06 +0800

    add GAUforTransfortmers class for transformers

--- attentionAligment/modules/GateAttentionUnit.py
+++ attentionAligment/modules/GateAttentionUnit.py
@@ -3,72 +3,47 @@
 # @Author : lovemefan
 # @Email : lovemefan@outlook.com
 # @File : GateAttentionUnit.py
-import math
 import torch
 import torch.nn.functional as F
 from torch import nn, einsum
 from einops import rearrange
 
 
-# helper functions
-
-def exists(val):
-    return val is not None
-
-def default(val, d):
-    return val if exists(val) else d
-
-def padding_to_multiple_of(n, mult):
-    remainder = n % mult
-    if remainder == 0:
-        return 0
-    return mult - remainder
-
-# scalenorm
-
-class ScaleNorm(nn.Module):
-    def __init__(self, dim, eps = 1e-5):
-        super().__init__()
-        self.scale = dim ** -0.5
-        self.eps = eps
-        self.g = nn.Parameter(torch.ones(1))
-
-    def forward(self, x):
-        norm = torch.norm(x, dim = -1, keepdim = True) * self.scale
-        return x / norm.clamp(min = self.eps) * self.g
-
-# absolute positional encodings
-
-
-
-
-
-# class
-
 class OffsetScale(nn.Module):
-    def __init__(self, dim, heads = 1):
+    def __init__(self, dim, heads=1):
         super().__init__()
         self.gamma = nn.Parameter(torch.ones(heads, dim))
         self.beta = nn.Parameter(torch.zeros(heads, dim))
-        nn.init.normal_(self.gamma, std = 0.02)
+        nn.init.normal_(self.gamma, std=0.02)
 
     def forward(self, x):
+        """
+        :param x: input
+        h： heads
+        d： dim
+        :return:
+        """
         out = einsum('... d, h d -> ... h d', x, self.gamma) + self.beta
-        return out.unbind(dim = -2)
+        return out.unbind(dim=-2)
 
-# gated attention unit
 
 class GAU(nn.Module):
+    """gated attention unit
+    implementation of the gated attention unit from
+    the paper Transformer Quality in Linear Time https://arxiv.org/pdf/2202.10447.pdf
+    and  https://github.com/lucidrains/FLASH-pytorch/blob/main/flash_pytorch/flash_pytorch.py
+    """
+
     def __init__(
         self,
         *,
         dim,
-        query_key_dim = 128,
-        expansion_factor = 2.,
-        add_residual = True,
-        causal = False,
-        dropout = 0.,
-        norm_klass = nn.LayerNorm
+        query_key_dim=128,
+        expansion_factor=2.,
+        add_residual=True,
+        causal=False,
+        dropout=0.,
+        norm_klass=nn.LayerNorm
     ):
         super().__init__()
         hidden_dim = int(expansion_factor * dim)
@@ -87,7 +62,7 @@ class GAU(nn.Module):
             nn.SiLU()
         )
 
-        self.offsetscale = OffsetScale(query_key_dim, heads = 2)
+        self.offsetscale = OffsetScale(query_key_dim, heads=2)
 
         self.to_out = nn.Sequential(
             nn.Linear(hidden_dim, dim),
@@ -99,31 +74,31 @@ class GAU(nn.Module):
     def forward(
         self,
         x,
-        rel_pos_bias = None,
-        mask = None
+        rel_pos_bias=None,
+        mask=None
     ):
         seq_len, device = x.shape[-2], x.device
 
         normed_x = self.norm(x)
-        v, gate = self.to_hidden(normed_x).chunk(2, dim = -1)
+        v, gate = self.to_hidden(normed_x).chunk(2, dim=-1)
 
-        qk = self.to_qk(normed_x)
-        q, k = self.offsetscale(qk)
+        z = self.to_qk(normed_x)
+        q, k = self.offsetscale(z)
 
-        sim = einsum('b i d, b j d -> b i j', q, k) / seq_len
+        qk = einsum('b i d, b j d -> b i j', q, k) / seq_len
 
-        if exists(rel_pos_bias):
-            sim = sim + rel_pos_bias
+        if rel_pos_bias is not None:
+            qk = qk + rel_pos_bias
 
-        attn = F.relu(sim) ** 2
+        attn = F.relu(qk) ** 2
         attn = self.dropout(attn)
 
-        if exists(mask):
+        if mask is not None:
             mask = rearrange(mask, 'b j -> b 1 j')
             attn = attn.masked_fill(~mask, 0.)
 
         if self.causal:
-            causal_mask = torch.ones((seq_len, seq_len), dtype = torch.bool, device = device).triu(1)
+            causal_mask = torch.ones((seq_len, seq_len), dtype=torch.bool, device = device).triu(1)
             attn = attn.masked_fill(causal_mask, 0.)
 
         out = einsum('b i j, b j d -> b i d', attn, v)
@@ -134,6 +109,11 @@ class GAU(nn.Module):
         if self.add_residual:
             out = out + x
 
-        return out
+        return out, attn
 
+
+class GAUForTransformers(GAU):
+    def __init__(self, config):
+        self.hidden_size = config.hidden_size
+        super().__init__(dim=self.hidden_size, dropout=config.hidden_dropout_prob)
 
